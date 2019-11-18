3da45d57cf5dc33f9d84b021c2fe9849d6355918
Marta Tost LLopis <marta.tost@gmail.com>
2019-11-18 11:53:07 +0100

    First Commit. Spring Boot project structure.

---
+++ src/main/java/com/mtllsoft/dicerollgame/DicerollgameApplication.java
@@ -0,0 +1,13 @@
+package com.mtllsoft.dicerollgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DicerollgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DicerollgameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mtllsoft/dicerollgame/DicerollgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtllsoft.dicerollgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DicerollgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
